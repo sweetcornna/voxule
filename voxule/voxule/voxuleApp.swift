51b339d6f6ba8875a8e368accf392acb8ea5ad02
Cornna <96944678+ymylive@users.noreply.github.com>
2026-05-22 13:07:08 +0800

    chore: 新增 voxule iOS 应用工程脚手架

---
+++ voxule/voxule/voxuleApp.swift
@@ -0,0 +1,17 @@
+//
+//  voxuleApp.swift
+//  voxule
+//
+//  Created by 喻永昌 on 2026/5/22.
+//
+
+import SwiftUI
+
+@main
+struct voxuleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ voxule/voxuleTests/voxuleTests.swift
@@ -0,0 +1,18 @@
+//
+//  voxuleTests.swift
+//  voxuleTests
+//
+//  Created by 喻永昌 on 2026/5/22.
+//
+
+import Testing
+
+struct voxuleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
